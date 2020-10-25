9a9fe48ba52b9a5f07d07962f6f853fa480fe4aa
Tsar-dev-collective <73416181+Tsar-dev-collective@users.noreply.github.com>
2020-10-25 00:19:09 -0500

    initial repository creation

---
+++ SteamGame/scripts/Boot.js
@@ -0,0 +1,42 @@
+//look familiar?
+var steamGame = steamGame || {};
+
+steamGame.Boot = function(){};
+
+steamGame.Boot.prototype = {
+    preload: function() {
+
+        //preload the loading screen stuff dfor the actual preload state later
+        this.load.image('logo', '/sprites/load/logo.png');
+        this.load.image('preloadbar', '/sprites/load/preloadbar.png');
+    },
+    create: function() {
+
+        //loading screen background (I chose a very very dark blue)
+        this.game.stage.backgroundColor = '#02052b';
+
+        //define a minimum and maximum dimension set so no matter what screen it's never weirdly stretched
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 240;
+        this.scale.minHeight = 170;
+        this.scale.maxWidth = 2880;
+        this.scale.maxHeight = 1920;
+
+        //center the game horizontally
+        this.scale.pageAlignHorizontally = true;
+
+        //set the game size automatically within the bounds from earlier
+        this.scale.setScreenSize(true);
+
+        //phaser has 3 physics engines available to us
+        //arcade, impact, and matter.js
+        //we'll be starting with arcade while we figure out the actually code to create the window and basic movement,
+        //then I'll probably switch us over to impact because of its advanced tile support
+        //we won't go to matter however, 
+        //matter is the vector graphics, particle system, springs and constraints answer to wed development
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //move to next state
+        this.state.start('Preload');
+    }
+};
