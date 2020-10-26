4cf344c32010dac61a87c4eb5291342d9bc55bd9
tonyroux <tonyrouxrito@yahoo.com>
2020-10-26 03:24:47 -0500

    I'm a little stupid, just a little

--- SteamGame/scripts/MainMenu.js
+++ SteamGame/scripts/MainMenu.js
@@ -2,7 +2,7 @@ steamGame.MainMenu = function() {};
 
 steamGame.MainMenu.prototype = {
     create: function() {
-        this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'sky');
+        this.game.stage.backgroundColor = '#aadbda';
 
         this.backBlimp = this.game.add.sprite(this.game.world.centerX + 192, this.game.world.centerY - 96, 'menuBack');
         this.backBlimp.anchor.setTo(0, 0.5);
