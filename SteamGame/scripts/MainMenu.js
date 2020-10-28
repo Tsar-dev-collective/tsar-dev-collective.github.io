ee5d251f41eeca1b47eb375eb9c809b893d3f6e5
tonyroux <tonyrouxrito@yahoo.com>
2020-10-28 14:17:29 -0500

    menu background and pan

--- SteamGame/scripts/MainMenu.js
+++ SteamGame/scripts/MainMenu.js
@@ -3,6 +3,8 @@ steamGame.MainMenu = function() {};
 steamGame.MainMenu.prototype = {
     create: function() {
         this.starting = false;
+        this.menustate = false;
+
         this.game.stage.backgroundColor = '#75a1a0';
 
         this.backBlimp = this.game.add.sprite(this.game.world.centerX + 192, (this.game.world.centerY / 2), 'menuBlimp');
@@ -12,7 +14,7 @@ steamGame.MainMenu.prototype = {
         this.backBlimp.animations.play('float', 8, true);
 
         this.backBall2 = this.game.add.sprite(75, this.game.world.height, 'menuBall2');
-        this.backBall2.anchor.setTo(0, 1);
+        this.backBall2.anchor.setTo(0, 0.95);
         this.backBall2.scale.setTo(0.2, 0.2);
         this.backBall2.animations.add('float');
         this.backBall2.animations.play('float', 6, true);
@@ -30,11 +32,10 @@ steamGame.MainMenu.prototype = {
             this['cloud' + i.toString()] = this.game.add.sprite(xAry(), yAry(), 'clouds');
             this['cloud' + i.toString()].frame = spriteAry();
             this['cloud' + i.toString()].anchor.setTo(1.1, 0.1);
-            //cloudMove(this['cloud' + i.toString()]);
         }
 
         this.backBall1 = this.game.add.sprite(100, this.game.world.height, 'menuBall1');
-        this.backBall1.anchor.setTo(0, 1);
+        this.backBall1.anchor.setTo(0, 0.95);
         this.backBall1.scale.setTo(0.3, 0.3);
         this.backBall1.animations.add('float', [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         this.backBall1.animations.play('float', 6, true);
@@ -62,6 +63,10 @@ steamGame.MainMenu.prototype = {
             this.titlePart10.frame = 10;
         this.titlePart11 = this.game.add.sprite(this.titlePart10.x + this.titlePart8.width - 24, this.titlePart3.y + this.titlePart3.height, 'title');
             this.titlePart11.frame = 11;
+
+        this.menuBack = this.game.add.sprite(this.game.world.centerX, this.game.world.height * 1.5, 'plaque');
+        this.menuBack.anchor.setTo(0.5, 0.5);
+        this.menuBack.scale.setTo(0.87, 0.8);
     },
 
     update: function() {
@@ -71,6 +76,11 @@ steamGame.MainMenu.prototype = {
           /*this.backBlimp.animations.pause('float');
           this.backBall1.animations.pause('float');
           this.backBall2.animations.pause('float');*/
+          //if (this.menustate == false) {
+              /*this.menuBack = this.game.add.sprite(this.game.world.centerX, (this.game.world.height * 1.5), 'plaque');
+              this.menuBack.anchor.setTo(0.5, 0);*/
+
+          //}
         }
 
         if (this.starting != true) {
@@ -105,40 +115,45 @@ steamGame.MainMenu.prototype = {
         if (this.starting == true) {
             if(this['cloud0'].x > 0) {
                 this['cloud0'].x -= 1;
-                this['cloud0'].y -= 0.5;
+                this['cloud0'].y -= 1.5;
             } else {
                 this['cloud0'].destroy();
             }
             if(this['cloud1'].x > 0) {
                 this['cloud1'].x -= 1;
-                this['cloud1'].y -= 0.5;
+                this['cloud1'].y -= 1.5;
             } else {
                 this['cloud1'].destroy();
             }
             if(this['cloud2'].x > 0) {
                 this['cloud2'].x -= 1;
-                this['cloud2'].y -= 0.5;
+                this['cloud2'].y -= 1.5;
             } else {
                 this['cloud2'].destroy();
             }
             if(this.backBlimp.y + this.backBlimp.height > 0) {
-                this.backBlimp.y -= 0.5;
+                this.backBlimp.y -= 1.5;
             } else {
                 this.backBlimp.destroy();
             }
+            if(this.menuBack.y > this.game.world.height / 2) {
+                this.backBall1.y -= 1.5;
+                this.backBall2.y -= 1.5;
+                this.menuBack.y -= 1.5;
+            }
             if(this.titlePart0.y + 600 > 0) {
-                this.titlePart0.y -= 0.5;
-                this.titlePart1.y -= 0.5;
-                this.titlePart2.y -= 0.5;
-                this.titlePart3.y -= 0.5;
-                this.titlePart4.y -= 0.5;
-                this.titlePart5.y -= 0.5;
-                this.titlePart6.y -= 0.5;
-                this.titlePart7.y -= 0.5;
-                this.titlePart8.y -= 0.5;
-                this.titlePart9.y -= 0.5;
-                this.titlePart10.y -= 0.5;
-                this.titlePart11.y -= 0.5;
+                this.titlePart0.y -= 1.5;
+                this.titlePart1.y -= 1.5;
+                this.titlePart2.y -= 1.5;
+                this.titlePart3.y -= 1.5;
+                this.titlePart4.y -= 1.5;
+                this.titlePart5.y -= 1.5;
+                this.titlePart6.y -= 1.5;
+                this.titlePart7.y -= 1.5;
+                this.titlePart8.y -= 1.5;
+                this.titlePart9.y -= 1.5;
+                this.titlePart10.y -= 1.5;
+                this.titlePart11.y -= 1.5;
             } else {
                 this.titlePart0.destroy();
                 this.titlePart1.destroy();
--- SteamGame/scripts/Preload.js
+++ SteamGame/scripts/Preload.js
@@ -24,6 +24,7 @@ steamGame.Preload.prototype = {
         this.load.atlasJSONHash('title', 'sprites/load/Title.png', 'sprites/load/Title.json');
             this.load.atlasJSONHash('menuBall2', 'sprites/images/hotairballoon2.png', 'sprites/images/hotairballoon2.json');
         this.load.atlasJSONHash('clouds', 'sprites/images/clouds.png', 'sprites/images/clouds.json');
+            this.load.image('plaque', 'maps/menuMapping.png');
     },
     create: function() {
         this.state.start('MainMenu');
