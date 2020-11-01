5194b8259cc989fbfc81d5529e31865d623a0384
thames-17 <ryancm0217@gmail.com>
2020-11-01 09:46:27 -0600

    i hope yall arent seeing this

--- SteamGame/scripts/game.js
+++ SteamGame/scripts/game.js
@@ -4,14 +4,12 @@ steamGame.Game = function(){}
 
 steamGame.Game.prototype = {
     create: function(){
-        //movement
+        //relevant keycodes
         wKey = this.game.input.keyboard.addKey(87)
         aKey = this.game.input.keyboard.addKey(65)
         sKey = this.game.input.keyboard.addKey(83)
         dKey = this.game.input.keyboard.addKey(68)
-        //start
         escapeKey = this.game.input.keyboard.addKey(27)
-        //select
         eKey = this.game.input.keyboard.addKey(69)
     },
     update: function(){}
