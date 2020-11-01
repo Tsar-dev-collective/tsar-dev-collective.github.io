319ed40825eb0a7f8abda235363b5c5a363e4603
thames-17 <ryancm0217@gmail.com>
2020-11-01 10:40:29 -0600

    key fix

--- SteamGame/scripts/game.js
+++ SteamGame/scripts/game.js
@@ -4,13 +4,15 @@ steamGame.Game = function(){}
 
 steamGame.Game.prototype = {
     create: function(){
-        //relevant keycodes
-        wKey = this.game.input.keyboard.addKey(87)
-        aKey = this.game.input.keyboard.addKey(65)
-        sKey = this.game.input.keyboard.addKey(83)
-        dKey = this.game.input.keyboard.addKey(68)
-        escapeKey = this.game.input.keyboard.addKey(27)
-        eKey = this.game.input.keyboard.addKey(69)
+        //movement 
+        upKey = this.game.input.keyboard.addKey(87) //w
+        leftKey = this.game.input.keyboard.addKey(65) //a
+        downKey = this.game.input.keyboard.addKey(83) //s
+        rightKey = this.game.input.keyboard.addKey(68) //d
+        //other interactivity
+        selectKey = this.game.input.keyboard.addKey(27) //escape
+        startKey = this.game.input.keyboard.addKey(69) // e
+        mapKey = this.game.input.keyboard.addKey(81)
     },
     update: function(){}
 }
