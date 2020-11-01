dfa420281576735457800097ef99cd1705329755
tonyroux <tonyrouxrito@yahoo.com>
2020-11-01 00:31:51 -0500

    made a mistake in documentation, my bad guys

--- SteamGame/scripts/game.js
+++ SteamGame/scripts/game.js
@@ -2,7 +2,7 @@ var steamGame = steamGame || {}
 
 steamGame.Game = function(){}
 
-steamGame.Game.prototype = function(){
-create: function(){}
-update: function(){}
+steamGame.Game.prototype = {
+    create: function(){},
+    update: function(){}
 }
