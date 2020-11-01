2467de78b8ccaaa157b79c31330ebdd3e11dbb37
thames-17 <ryancm0217@gmail.com>
2020-11-01 09:43:40 -0600

    oops

--- SteamGame/scripts/game.js
+++ SteamGame/scripts/game.js
@@ -3,7 +3,17 @@ var steamGame = steamGame || {}
 steamGame.Game = function(){}
 
 steamGame.Game.prototype = {
-    create: function(){},
+    create: function(){
+        //movement
+        wKey = this.game.input.keyboard.addKey(87)
+        aKey = this.game.input.keyboard.addKey(65)
+        sKey = this.game.input.keyboard.addKey(83)
+        dKey = this.game.input.keyboard.addKey(68)
+        //start
+        escapeKey = this.game.input.keyboard.addKey(27)
+        //select
+        eKey = this.game.input.keyboard.addKey(69)
+    },
     update: function(){}
 }
 
