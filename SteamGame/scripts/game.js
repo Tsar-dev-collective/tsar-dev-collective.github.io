790b9fad780caf51bb6b905e5973be966eeb1e4f
thames-17 <ryancm0217@gmail.com>
2020-11-01 09:44:21 -0600

    I am illiterate

--- SteamGame/scripts/game.js
+++ SteamGame/scripts/game.js
@@ -16,13 +16,3 @@ steamGame.Game.prototype = {
     },
     update: function(){}
 }
-
-//movement
-wKey = this.game.input.keyboard.addKey(87)
-aKey = this.game.input.keyboard.addKey(65)
-sKey = this.game.input.keyboard.addKey(83)
-dKey = this.game.input.keyboard.addKey(68)
-//start
-escapeKey = this.game.input.keyboard.addKey(27)
-//select
-eKey = this.game.input.keyboard.addKey(69)
